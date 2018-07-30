8ccd9c83233f17fcc767916901ef282273411bb0
suil <lduarte.suil@gmail.com>
2018-07-30 12:43:57 +0100

    new dist

--- bin/context-service.js
+++ bin/context-service.js
@@ -1,6 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.contextService = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // version: 0.12.0
-// date: Fri Jul 27 2018 14:40:15 GMT+0100 (Western European Summer Time)
+// date: Mon Jul 30 2018 12:30:20 GMT+0100 (Western European Summer Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -27,7 +27,7 @@
 
 
 // version: 0.12.0
-// date: Fri Jul 27 2018 14:40:15 GMT+0100 (Western European Summer Time)
+// date: Mon Jul 30 2018 12:30:21 GMT+0100 (Western European Summer Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -750,7 +750,7 @@ function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});va
 t.exports=e.default}])});
 },{}],2:[function(require,module,exports){
 // version: 0.12.0
-// date: Fri Jul 27 2018 14:40:15 GMT+0100 (Western European Summer Time)
+// date: Mon Jul 30 2018 12:30:20 GMT+0100 (Western European Summer Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -777,7 +777,7 @@ t.exports=e.default}])});
 
 
 // version: 0.12.0
-// date: Fri Jul 27 2018 14:40:15 GMT+0100 (Western European Summer Time)
+// date: Mon Jul 30 2018 12:30:21 GMT+0100 (Western European Summer Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
